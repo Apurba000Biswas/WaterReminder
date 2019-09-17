8da8397fa67692259c8378ef7b5c01f4ac044311
Apurba <apurba10024@gmail.com>
2019-09-17 15:38:07 +0600

    1.6 Added WaterReminderFirebaseJobService

--- app/src/main/java/com/apurba/waterreminder/sync/WaterReminderFirebaseJobService.java
+++ app/src/main/java/com/apurba/waterreminder/sync/WaterReminderFirebaseJobService.java
@@ -1,18 +1,39 @@
 package com.apurba.waterreminder.sync;
 
-import androidx.annotation.NonNull;
+import android.content.Context;
+import android.os.AsyncTask;
 
 import com.firebase.jobdispatcher.JobParameters;
 import com.firebase.jobdispatcher.JobService;
 
+import androidx.annotation.NonNull;
+
 public class WaterReminderFirebaseJobService extends JobService {
+    private AsyncTask mBackgroundTask;
+
     @Override
-    public boolean onStartJob(@NonNull JobParameters job) {
-        return false;
+    public boolean onStartJob(@NonNull final JobParameters job) {
+        mBackgroundTask = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] objects) {
+                Context context = WaterReminderFirebaseJobService.this;
+                ReminderTasks.executeTask(context, ReminderTasks.ACTION_CHARGING_REMINDER);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Object o) {
+                jobFinished(job, false);
+            }
+        };
+
+        mBackgroundTask.execute();
+        return true;
     }
 
     @Override
     public boolean onStopJob(@NonNull JobParameters job) {
-        return false;
+        if (mBackgroundTask != null)  mBackgroundTask.cancel(true);
+        return true;
     }
 }
